cbda6ea4a59470c0790186a1a8f4a57e4dae9489
Thiago Adriano <tadriano@band.com.br>
2018-02-28 11:37:17 -0300

    add projects

--- index.ts
+++ index.ts
@@ -1,21 +1,32 @@
+import * as bodyparser from 'body-parser';
 import * as express from 'express'
 import * as multer from 'multer'
 import uploads from './upload'
 
 const app = express();
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
 app.post('/', uploads.single('avatar'), (req, res) => {
-    try {
-        res.send('Arquivo enviado com sucesso: ' + req.file.filename);
-    } catch (error) {
-        console.log(error);
-    }
 
+
+    if (!req.file) {
+        console.log("No file received");
+        return res.send({
+            success: false
+        });
+
+    } else {
+        console.log('file received');
+        return res.send({
+            success: true
+        })
+    }
 })
 
 app.listen(3000, function () {
--- upload.ts
+++ upload.ts
@@ -1,6 +1,7 @@
 import * as multer from "multer";
 
 const storage = multer.diskStorage({
+
     destination: function (req, file, cb) {
         cb(null, "uploads/");
     },
